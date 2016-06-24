bc988369dea017d58d911e92c98a22873fe3e5ee
Carsten <Carsten.Huth@checkmarx.com>
2016-06-24 20:49:16 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express'),
+	app = express();
+	
+app.get('/', function(req, res) {
+	res.send('hello');
+});
+
+app.listen(3000, function() {
+	console.log('listening on port 3000');
+});
+
